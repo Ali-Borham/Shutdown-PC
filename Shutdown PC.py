39d6a2d69449e648e674c11ebc20c3702632df4a
Ali Borham <103311893+Ali-Borham@users.noreply.github.com>
2022-04-13 22:12:21 +0200

    Update Shutdown PC.py

--- Shutdown PC.py
+++ Shutdown PC.py
@@ -1,31 +1,5 @@
-### Shutdown PC Programme ###
-# auto-py-to-exe
-
 import os
 from threading import Timer 
-
-# os.system("shutdown /s /t 1")
-
-print ("                  ****************************            ")
-print ("                  ****************************            ")
-print ("     <<<<<< Welcome to (Ali Borham's Shutdown PC App.) >>>>>>"    )
-print (" ")
-print ("<<< You can Shutdown your PC after any time you want >>>")
-print (" ")
-print ("                  ****************************            ")
-print ("                  ****************************            ")
-print (" ")
-
-
-# time_minutes= float (input ("After how many 'minutes' do you want to shutdown your PC? =>> "))
-
-# def shutdown():
-#     os.system("shutdown /s /t 1")
-
-# The_timer= Timer (time_minutes * 60 , shutdown)
-# The_timer.start()
-
-#############################################################################################################
 
 while True:
     try:
@@ -39,6 +13,3 @@ while True:
         print("!!!!!")
         print("Please type a minute.")
         print("-------------------------------")
-    
-#############################################################################################################
-
